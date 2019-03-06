35844f5432a319be925dd2e6cad86e3ad9b74f2c
Jason Anderson <sirius-c@iname.com>
2019-03-05 18:12:00 -0600

    Removed references to background music and title screen.

--- Scenes/Title Screen.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-onready var bg_music = get_node("BG music")
-
-func _ready():
-	bg_music.play()
-
-func _on_Start_game_pressed():
-	get_tree().change_scene("res://Scenes/Input_Handler.tscn")
