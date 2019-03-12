0ae89c96535d678e4e90e4204e95f3aa5f5592c3
Jason Anderson <sirius-c@iname.com>
2019-03-12 02:30:22 -0500

    Added button to "About" panel because it didn't go away by tapping the screen.

--- Scenes/About.gd
+++ Scenes/About.gd
@@ -10,7 +10,10 @@ func _on_About_button_pressed():
 		self.hide()
 
 # The "About box" must disappear when the screen is pressed.
-func _input(event):
-	if event.type == InputEvent.SCREEN_TOUCH and event.is_pressed() and self.is_visible():
-		self.hide()
+#func _input(event):
+#	if event.type == InputEvent.SCREEN_TOUCH and event.is_pressed() and self.is_visible():
+#		self.hide()
 
+# The "About box" must disappear when the screen is pressed.
+func _on_Okay_pressed():
+	self.hide()
