f7aed471eaa736e8915c2ea429497f73196fa210
Jason Anderson <sirius-c@iname.com>
2019-03-12 13:22:42 -0500

    Added new music

--- bg_music.gd
+++ bg_music.gd
@@ -3,22 +3,24 @@ extends StreamPlayer
 var bg_music_stream = ['res://Music/copycat.ogg', 'res://Music/happy bgm.ogg', 'res://Music/endrit_tone.ogg',
 						'res://Music/Bouncy.ogg', 'res://Music/Moving higher.ogg','res://Music/Neon Runner.ogg',
 						"res://Music/That's it for today (LOOP).ogg", "res://Music/Mandatory Overtime.ogg",
-						"res://Music/Clouds.ogg", "res://Music/Cue.ogg", "res://Music/bunumh.ogg"]
+						"res://Music/Clouds.ogg", "res://Music/Cue.ogg", "res://Music/bunumh.ogg",
+						"res://Music/n os.ogg"]
 
 func _ready():
 	# Set the mood for each level! Match the music to each color (the first color code in the array).
 	# Here's how the music breaks down:
 	#	'cea7e5' => 'That's it for today.ogg'
 	#   'f5fd99' => 'copycat.ogg'
-	#   'b7ecfe' =>'happy bgm.ogg'
+	#   'b7ecfe' => 'happy bgm.ogg'
 	#   'fd9e9e' => 'Bouncy.ogg'
 	#   'd8d8d8' => 'endrit_tone.ogg'
-	#   'DB9DC5' => 'Moving higher.ogg'
+	#   'db9dc5' => 'Moving higher.ogg'
 	#	'fcfff6' => 'Neon Runner.ogg'
     #   '93eebd' => 'Mandatory Overtime.ogg'
 	#	'fbfdf0' => 'Clouds.ogg'
 	#	'27ebe8' => 'Cue.ogg'
 	#	'334455' => 'bunumh.ogg'
+	#	'df3819' => 'n os.ogg'
 	#	'8b64fa' => '?'
 	if global.current_palette[0] == 'f5fd99':
 		var current_music = load( bg_music_stream[0] )
@@ -32,7 +34,7 @@ func _ready():
 	elif global.current_palette[0] == 'd8d8d8':
 		var current_music = load( bg_music_stream[3] )
 		set_stream(current_music)
-	elif global.current_palette[0] == 'DB9DC5':
+	elif global.current_palette[0] == 'db9dc5':
 		var current_music = load( bg_music_stream[4] )
 		set_stream( current_music )
 	elif global.current_palette[0] == 'fcfff6':
@@ -53,11 +55,18 @@ func _ready():
 	elif global.current_palette[1] == 'ee0000':
 		var current_music = load( bg_music_stream[10] )
 		set_stream( current_music )
+	elif global.current_palette[0] == 'df3819':
+		var current_music = load( bg_music_stream[11] )
+		set_stream( current_music )
 	else:
+		_print_palette(global.current_palette)
 		var current_music = load( bg_music_stream[0] )
 		set_stream(current_music)
 	play()
 	set_process( true )
+
+func _print_palette(palette):
+	print( "The current palette is [%s, %s, %s]." % [ palette[0], palette[1], palette[2] ] )
 
 func _process(delta):
 	if not global.sound_enabled and is_playing():
